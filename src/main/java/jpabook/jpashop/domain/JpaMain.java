3a8626179a410c9358dc48da0cf5491224944788
Citrons06 <yeolee1212@gmail.com>
2023-11-01 16:39:24 +0900

    study1101-1

--- src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,9 +17,9 @@ public class JpaMain {
         tx.begin();  //트랜잭션 시작
 
         try {
-
             Team team = new Team();
             team.setName("team");
+            //team.getMembers().add(member);
             em.persist(team);
 
             Member member = new Member();
@@ -26,13 +27,14 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            em.flush();
+            em.clear();
 
-            Team newTeam = em.find(Team.class, 3L);
-            findMember.setTeam(newTeam);
-
+            Team findTeam = em.find(Team.class, team.getMembers());
+            List<Member> members = findTeam.getMembers();
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
